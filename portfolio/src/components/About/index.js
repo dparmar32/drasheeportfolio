6704c143a8b118c0df8cd5009d2be17f0f8bb4db
Drashtee <drashteeparmar@gmail.com>
2022-03-30 18:09:46 -0400

    updated about and contact

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="my-5">
+            <div className="my-2">
+                <div className="profile-img my-5">
+                    <i className="fas fa-user-circle" style={{fontSize: "96px"}}></i>
+                </div>
+                <p>
+                    An experienced Software Engineer with a demonstrated background in designing and implementing
+                    software systems and applications. A keen ability to work with developers to produce intellectual
+                    property that meets design schedules and customer functional and quality requirements. Experienced
+                    with the front-end application design as well as back-end verification and validation. Developed
+                    code in line with Agile and Test-Driven Development Methodologies. </p>
+
+            </div>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import Header from "./components/Header";
+import Nav from "./src/components/Nav";
+import Page from "./src/components/Page";
+import Footer from "./src/components/Footer";
+
+function App() {
+    const [pages] = useState([
+        {name: "about me"},
+        {name: "portfolio"},
+        {name: "contact"},
+        {name: "resume"}
+    ]);
+
+    const [currentPage, setCurrentPage] = useState(pages[0]);
+
+    return (
+        <div>
+            <Header>
+                <Nav
+                    pages={pages}
+                    setCurrentPage={setCurrentPage}
+                    currentPage={currentPage}
+                ></Nav>
+            </Header>
+            <main>
+                <Page currentPage={currentPage}></Page>
+            </main>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
